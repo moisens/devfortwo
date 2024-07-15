b9d2575a57dc5375eb1d90cc403f7040ee5cc1ec
Moise <moise_ns@outlook.com>
2024-07-16 00:19:59 +0200

    modified favicon + utils folder & links

---
+++ utils/links.ts
@@ -0,0 +1,25 @@
+export type NavLinksType = {
+  id: string;
+  href: string;
+  label: string;
+};
+
+const links: NavLinksType[] = [
+  {
+    id: "ZcrauFdUHO2eIbBw",
+    href: "/add-job",
+    label: "add job",
+  },
+  {
+    id: "R0LWqNe6KVbXpOHh",
+    href: "/jobs",
+    label: "jobs",
+  },
+  {
+    id: "XTpY0DRj4SncAmJe",
+    href: "/stats",
+    label: "stats",
+  },
+];
+
+export default links;
