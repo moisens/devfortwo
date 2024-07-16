603a3d7cd9206c1bd4896ba739f9d26814700712
Moise <moise_ns@outlook.com>
2024-07-16 13:49:48 +0200

    links dropdown functionality

--- components/linksdropdown/LinksDropdown.tsx
+++ components/linksdropdown/LinksDropdown.tsx
@@ -1,7 +1,43 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { AlignLeft } from "lucide-react";
+import links from "@/utils/links";
+import Link from "next/link";
+
 type Props = {};
 
 const LinksDropdown = (props: Props) => {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          const { id, href, label } = link;
+          return (
+            <DropdownMenuItem key={id}>
+              <Link href={href}>{label}</Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -2,27 +2,33 @@ import Image from "next/image";
 //import Logo from "@/assets/logo.svg";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import LinksDropdown from "../linksdropdown/LinksDropdown";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
   return (
-    <nav className="bg-slate-200 sticky top-0 px-3 py-2 flex">
+    <nav className="bg-muted sticky top-0 px-3 py-2 flex">
       <div className="flex flex-1 items-center gap-4">
         <ul>
           <li>
             <Link href="/">devfortwo</Link>
           </li>
         </ul>
-        <div className="w-12 h-12 bg-slate-600 rounded-lg flex justify-center items-center">
-          1
-        </div>
+        <LinksDropdown />
       </div>
       <div className="flex justify-center items-center gap-2">
         <div className="w-12 h-12 bg-slate-600 rounded-lg flex justify-center items-center">
           2
         </div>
-        <Button asChild>
-          <Link href="/sign-in">login</Link>
-        </Button>
+        {!user ? (
+          <Button asChild>
+            <Link href="/sign-in">login</Link>
+          </Button>
+        ) : (
+          <UserButton />
+        )}
       </div>
     </nav>
   );
