e0eb9e7a6ed297c24842e1af726de47ea2d1afa6
Moise <moise_ns@outlook.com>
2024-07-16 12:00:52 +0200

    dashboard layout modif + sidebar modif

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,12 +6,12 @@ import SideBar from "@/components/sidebar/SideBar";
 const layout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <Navbar />
       <main className="grid lg:grid-cols-5">
         <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
           <SideBar />
         </div>
         <div className="lg:col-span-4">
+          <Navbar />
           <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
         </div>
       </main>
--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import links from "@/utils/links";
+import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
 type Props = {};
 
 const SideBar = (props: Props) => {
-  return <aside>SideBar</aside>;
+  const pathname = usePathname();
+
+  return (
+    <aside className="py-4 px-8 bg-muted h-full">
+      <div className="flex flex-col mt-20 gap-4">
+        {links.map((link) => {
+          const { id, href, label } = link;
+          return (
+            <Button
+              asChild
+              key={id}
+              variant={pathname === `${href}` ? "outline" : "ghost"}
+              className="flex justify-start items-center capitalize"
+            >
+              <Link href={href}>{label}</Link>
+            </Button>
+          );
+        })}
+      </div>
+    </aside>
+  );
 };
 
 export default SideBar;
