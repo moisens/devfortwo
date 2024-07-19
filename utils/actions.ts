dc64bad0b9ab59c2021bd046f49d04a826a7868d
Moise <moise_ns@outlook.com>
2024-07-19 22:46:55 +0200

    actions get all jobs

--- utils/actions.ts
+++ utils/actions.ts
@@ -32,3 +32,58 @@ export const createJobActions = async (
     return null;
   }
 };
+
+export type GetAllJobsType = {
+  jobs: [];
+  count: number;
+  page: number;
+  totalPages: number;
+};
+
+type GetAllJobsActionTypes = {
+  search?: string;
+  jobStatus?: string;
+  page?: number;
+  limit?: number;
+};
+
+export const getAllJobsActions = async ({
+  search,
+  jobStatus,
+  page = 1,
+  limit = 10,
+}: GetAllJobsActionTypes): Promise<GetAllJobsType> => {
+  const userId = await authenticateAndRedirect();
+
+  try {
+    let whereClause: Prisma.JobWhereInput = {
+      clerkId: userId,
+    };
+    if (search) {
+      whereClause = {
+        ...whereClause,
+        OR: [
+          { position: { contains: search } },
+          { company: { contains: search } },
+        ],
+      };
+    }
+
+    if (jobStatus && jobStatus !== "all") {
+      whereClause = {
+        ...whereClause,
+        status: jobStatus,
+      };
+    }
+
+    const jobs: JobType[] = await prisma.job.findMany({
+      where: whereClause,
+      orderBy: { createdAt: "desc" },
+    });
+
+    return { jobs: [], count: 0, page: 1, totalPages: 0 };
+  } catch (error) {
+    console.log(error);
+  }
+  return { jobs: [], count: 0, page: 1, totalPages: 0 };
+};
