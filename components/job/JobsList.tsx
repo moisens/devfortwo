62db56ebcc6598a0fd0233d1dfd4db2c6d9bf241
Moise <moise_ns@outlook.com>
2024-07-20 21:13:38 +0200

    JobList functionality + adding badge and separator components in ui

--- components/job/JobsList.tsx
+++ components/job/JobsList.tsx
@@ -1,7 +1,36 @@
-type Props = {};
+"use client";
 
-const JobsList = (props: Props) => {
-  return <div>JobsList</div>;
+import { getAllJobsActions } from "@/utils/actions";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import JobCard from "./JobCard";
+
+const JobsList = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+  const pageNumber = Number(searchParams.get("page") || 1);
+
+  const { data, isPending } = useQuery({
+    queryKey: ["jobs", search ?? "", "jobStatus", "page"],
+    queryFn: () => getAllJobsActions({ search, jobStatus, page: pageNumber }),
+  });
+
+  const jobs = data?.jobs || [];
+
+  if (isPending) return <h2 className="text-xl">Please wait...</h2>;
+  if (jobs.length < 1) return <h2 className="text-xl">No Jobs found!</h2>;
+
+  return (
+    <>
+      <div className="grid md:grid-cols-2  gap-8">
+        {jobs.map((job) => {
+          const { id } = job;
+          return <JobCard key={id} job={job} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default JobsList;
