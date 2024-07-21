b62b85e179a11b5c21f8fff7ab3c470a066789a7
Moise <moise_ns@outlook.com>
2024-07-21 13:04:45 +0200

    delete job actions func + delete job func on the front

--- components/job/DeleteJobBtn.tsx
+++ components/job/DeleteJobBtn.tsx
@@ -1,7 +1,43 @@
-type Props = {};
+import { deleteJobActions } from "@/utils/actions";
+import JobInfo from "./JobInfo";
+import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
+import { useToast } from "../ui/use-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const DeleteJobBtn = (props: Props) => {
-  return <div>DeleteJobBtn</div>;
+export type DeleteJobType = {
+  id: string;
+};
+
+const DeleteJobBtn = ({ id }: DeleteJobType) => {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (id: string) => deleteJobActions(id),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({ description: "There was an error! ☝🏼" });
+        return;
+      }
+
+      queryClient.invalidateQueries({ queryKey: ["jobs"] });
+      queryClient.invalidateQueries({ queryKey: ["stats"] });
+      queryClient.invalidateQueries({ queryKey: ["charts"] });
+    },
+  });
+
+  return (
+    <Button
+      size="sm"
+      disabled={isPending}
+      onClick={() => {
+        mutate(id);
+      }}
+    >
+      {isPending ? "deleting..." : "delete"}
+    </Button>
+  );
 };
 
 export default DeleteJobBtn;
