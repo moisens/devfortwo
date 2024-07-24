e0520043ad1aa51dad54f239894737dd6f182f44
Moise <moise_ns@outlook.com>
2024-07-24 17:25:15 +0200

    btn container functionality

--- components/buttoncontainer/ButtonContainer.tsx
+++ components/buttoncontainer/ButtonContainer.tsx
@@ -9,7 +9,37 @@ export type ButtonContainerProps = {
 };
 
 const ButtonContainer = ({ currentPage, totalPages }: ButtonContainerProps) => {
-  return <h2 className="text-4xl">button container</h2>;
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const pageButtons = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  const handlePageChange = (page: number) => {
+    const defaultParams = {
+      search: searchParams.get("search") || "",
+      jobStatus: searchParams.get("jobStatus") || "",
+      page: String(page),
+    };
+
+    let params = new URLSearchParams(defaultParams);
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <div className="flex gap-x-2">
+      {pageButtons.map((page) => (
+        <Button
+          key={page}
+          size="icon"
+          variant={currentPage === page ? "default" : "outline"}
+          onClick={() => handlePageChange(page)}
+        >
+          {page}
+        </Button>
+      ))}
+    </div>
+  );
 };
 
 export default ButtonContainer;
